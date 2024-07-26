a8e429cc4ff16517f8959d4a1a9c87056da8b638
Movin Tilakaratne <movin1999t@gmail.com>
2024-07-26 22:45:47 +0530

    Add mock data

---
+++ app/lib/definitions.ts
@@ -0,0 +1,20 @@
+// Contains interface definitions for the data.
+
+export enum Category {
+    BusinessAndMoney = "Business & Money",
+    PositivityAndLifeHacks = "Positivity & Life Hacks",
+    SelfHelp = "Self Help"
+}
+
+export interface Books {
+    isbn: string; // Unique identifier
+    title: string;
+    author: string;
+    publisher: string;
+    publicationYear: number;
+    summary: string;
+    imageUrl: string;
+    price: number;  // Store price in cents
+    quantityInStock: number;
+    category: Category;
+}
